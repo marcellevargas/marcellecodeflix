e0be47613d7eb1b0e6564ec0b73b57506199c90c
marcellevargas <marcelle.vargas96@gmail.com>
2020-07-30 22:03:02 -0300

    Imersão alura

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import Logo from '../../assets/img/marcellecodeflix.png';
-import './Menu.css';
-import Button from '../ButtonLink/index';
+import React from "react";
+import Logo from "../../assets/img/marcellecodeflix.png";
+import "./Menu.css";
+import Button from "../ButtonLink/index";
+import { Link } from "react-router-dom";
+
 function Menu() {
-    return (
-        <nav className="Menu">
-            <a href="/">                
-                <img className="Logo" src={Logo} alt="MarcelleCode flix logo"/>
-            </a>
+  return (
+    <nav className='Menu'>
+      <Link to='/'>
+        <img className='Logo' src={Logo} alt='MarcelleCode flix logo' />
+      </Link>
 
-            <Button className="ButtonLink" href="/">                
-                Hello World
-            </Button>
-
-        </nav>
-    );
+      <Button as={Link} className='ButtonLink' to='/cadastro/video'>
+        Cadastrar video
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./pages/Home/index.js";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CadastroVideo from "./pages/Cadastro/Video";
+import CadastroCategoria from "./pages/Cadastro/Categoria";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route path='/cadastro/video' component={CadastroVideo} />
+      <Route path='/cadastro/categoria' component={CadastroCategoria} />
+      <Route path='/' component={App} exact />
+      <Route component={() => <div>Página 404</div>} />
+    </Switch>
+  </BrowserRouter>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  document.getElementById("root")
 );
